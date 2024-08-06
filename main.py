1071d169ef4b5630e818213a747bb4f6b93b2bee
Gabriel Acosta <gabriacos@gmail.com>
2024-08-05 23:39:39 -0300

    feat: :zap: AGREGAR graficos de mathplolib
    Se agregó las funciones para la visualizaciones de graficos en mathplolib sobre perfomance_score, salary

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from models import employee_perfomance
 from utils.add_data_to_mysql import add_data_to_mysql
 from functions.function_to_calculate_medians import * 
 from functions.function_correlation import *
+from functions.function_to_matplolib import *
+
 if __name__ == "__main__":
     add_data_to_mysql()
     print("Data added to MySQL")
@@ -19,11 +21,16 @@ if __name__ == "__main__":
     print("Calculating std of salary")
     function_to_calculate_std_salary()
     print("All calculations done")
-#         #calculamos la correlacion entre performance_score y salary
+    #calculamos la correlacion entre performance_score y salary
     print("Calculating correlation between performance_score and salary")
     function_correlation_salary()
     function_correlation_years()
     print("All calculations done")
+    #graficamos los datos
+    histograma_performance_score_department()
+    scatterplot_years_with_company_vs_performance_score()
+    scatterplot_salary_vs_performance_score()
+    print("All graphs done")
 
 
 
